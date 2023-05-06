6eb64859b66c9832b8f7f6d01fa07c42f7aec63b
devid <devid367552@gmail.com>
2023-05-06 09:30:39 +0300

    RecordsClasses

---
+++ src/OOP/RecordsClasses.java
@@ -0,0 +1,12 @@
+/**
+ * Что такое Record в Java 16? Record — это новый тип объявления (type of declaration) для определения неизменяемых (immutable) классов данных.
+ * Применение Record вместо обычного класса позволяет избежать рутинного написания методов hashCode(), equals(), toString(), геттеров и конструктора.
+ * @link - https://metanit.com/java/tutorial/3.18.php
+ */
+
+
+package OOP;
+
+public class RecordsClasses {
+
+}
